3a36588cc7ee7b04c165140441edfd311a905bb9
AlexMiesen <alex@i-motor.com.au>
2022-08-31 23:08:06 +1000

    Added switch, this needs to have a break otherwise it will include all the other days in our example(day 4, would also include 5,6 and 7)

--- app.js
+++ app.js
@@ -57,3 +57,30 @@ if (new_password.length >= 8 && new_password.indexOf(' ') === -1) {
 	console.log("Invalid Password")
 }
 
+//example 6
+
+day = 4
+
+switch(day) {
+	case 1: 
+		console.log("MONDAY") 
+		break
+	case 2: 
+		console.log("TUESDAY") 
+		break
+	case 3: 
+		console.log("WEDSDAY") 
+		break
+	case 4: 
+		console.log("THURSDAY") 
+		break
+	case 5: 
+		console.log("FRIDAY") 
+		break
+	case 6: 
+		console.log("SATURDAY") 
+		break
+	case 7: 
+		console.log("SUNDAY") 
+		break
+}
